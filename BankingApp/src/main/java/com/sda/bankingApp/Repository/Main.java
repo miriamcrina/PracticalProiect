35547069518a67c16aa83083c40b20d728f1a2a5
Miriam <miriamcrina@yahoo.com>
2020-10-11 19:01:10 +0300

    Third commit - 11/10/20 -associations + 1 method

---
+++ BankingApp/src/main/java/com/sda/bankingApp/Repository/Main.java
@@ -0,0 +1,9 @@
+package com.sda.bankingApp.Repository;
+
+public class Main {
+    public static void main(String[] args) {
+        Repository repository = new Repository();
+
+        repository.register();
+    }
+}
--- BankingApp/src/main/java/com/sda/bankingApp/Repository/Repository.java
+++ BankingApp/src/main/java/com/sda/bankingApp/Repository/Repository.java
@@ -15,7 +15,7 @@ public class Repository {
 
     public void register() {
 
-        int check = 0;
+
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession();
              Scanner scanner = new Scanner(System.in)) {
@@ -28,10 +28,10 @@ public class Repository {
 
             if (firstName == null || firstName.isEmpty()) {
                 logger.warning("This field should not be empty!\n");
-                check = 1;
+
             } else if (!firstName.matches("^[a-zA-Z]+$")) {
                 logger.warning("This field is invalid\n");
-                check = 1;
+
             }
 
             logger.info("Last name: " );
@@ -39,37 +39,37 @@ public class Repository {
 
             if (lastName == null || lastName.isEmpty()) {
                 logger.warning("This field should not be empty!\n");
-                check = 1;
+
             } else if (!lastName.matches("^[a-zA-Z]+$")) {
                 logger.warning("This field is invalid\n");
-                check = 1;
+
             }
 
             logger.info("Social Security Number: " );
             String ssn = scanner.nextLine();
             if (ssn == null || ssn.isEmpty()) {
                 logger.warning("This field should not be empty!\n");
-                check = 1;
+
             } else if (ssn.matches("^[0-9]+$")) {
                 if (!(ssn.length() == 13)) {
                     logger.warning("Social Security Number should be 13 digits.\n");
-                    check = 1;
+
                 }
 
             } else if (!ssn.matches("^[0-9]+$")) {
                 logger.warning("Invalid Social Security Number");
-                check = 1;
+
             }
 
-            //pass
+
             logger.info("Email address: " );
             String email = scanner.nextLine();
             if (email == null || email.isEmpty()) {
                 logger.warning("This field should not be empty!\n");
-                check = 1;
+
             } else if (!email.matches("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")) {
                 logger.warning("This field is invalid\n");
-                check = 1;
+
             }
 
             logger.info("Username: " );
@@ -77,10 +77,10 @@ public class Repository {
 
             if (username == null || username.isEmpty()) {
                 logger.warning("This field should not be empty!\n");
-                check = 1;
+
             } else if (!username.matches("^[a-zA-Z0-9]+$")) {
                 logger.warning("This field is invalid\n");
-                check = 1;
+
             }
 
 
@@ -88,24 +88,18 @@ public class Repository {
             String password = scanner.nextLine();
             if (password == null || password.isEmpty()) {
                 logger.warning("This field should not be empty!\n");
-                check = 1;
+
             } else if (password.matches("^[a-zA-Z0-9]+$")) {
                 if ((password.length() < 6)) {
                     logger.warning("Password is too short. It should contain at least 6 characters.\n");
-                    check = 1;
+
                 }
 
             } else if (!password.matches("^[0-9]+$")) {
                 logger.warning("Invalid Password");
-                check = 1;
             }
 
-//            if (check == 0)
-//                return 0;
-//            else {
-//                JOptionPane.showMessageDialog(null, errorset);
-//                return 1;
-//            }
+
            Customer customer = new Customer(firstName, lastName, ssn, email, username, password);
 
             session.save(customer);
