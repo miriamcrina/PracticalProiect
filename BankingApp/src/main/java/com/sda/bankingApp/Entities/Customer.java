feab36be222d79a76c11a72589f709472335ef91
Miriam <miriamcrina@yahoo.com>
2020-10-11 11:32:13 +0300

    Initial commit

---
+++ BankingApp/src/main/java/com/sda/bankingApp/Entities/Customer.java
@@ -0,0 +1,11 @@
+package com.sda.bankingApp.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity(name = "Customer")
+@Table(name = "customer")
+public class Customer {
+
+}
