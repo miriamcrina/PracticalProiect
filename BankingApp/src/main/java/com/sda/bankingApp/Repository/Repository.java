53382625fb71227f5c4e733344266611aa408b57
Miriam <miriamcrina@yahoo.com>
2020-10-12 18:24:02 +0300

    12/10/20 -associations + 1 method - error in method

--- BankingApp/src/main/java/com/sda/bankingApp/Repository/Repository.java
+++ BankingApp/src/main/java/com/sda/bankingApp/Repository/Repository.java
@@ -15,89 +15,146 @@ public class Repository {
 
     public void register() {
 
-
         Transaction transaction = null;
+
         try (Session session = HibernateUtil.getSessionFactory().openSession();
              Scanner scanner = new Scanner(System.in)) {
 
+            String emptyMessage = "This field should not be empty!\n";
+            String invalidMessage = "This field is invalid\n";
+            boolean userInputCorrect = false;
+
             transaction = session.beginTransaction();
-            //id
             logger.info("Introduce your credentials:");
-            logger.info("First name: " );
-            String firstName = scanner.nextLine();
-
-            if (firstName == null || firstName.isEmpty()) {
-                logger.warning("This field should not be empty!\n");
-
-            } else if (!firstName.matches("^[a-zA-Z]+$")) {
-                logger.warning("This field is invalid\n");
-
-            }
-
-            logger.info("Last name: " );
-            String lastName = scanner.nextLine();
-
-            if (lastName == null || lastName.isEmpty()) {
-                logger.warning("This field should not be empty!\n");
-
-            } else if (!lastName.matches("^[a-zA-Z]+$")) {
-                logger.warning("This field is invalid\n");
-
-            }
-
-            logger.info("Social Security Number: " );
-            String ssn = scanner.nextLine();
-            if (ssn == null || ssn.isEmpty()) {
-                logger.warning("This field should not be empty!\n");
-
-            } else if (ssn.matches("^[0-9]+$")) {
-                if (!(ssn.length() == 13)) {
-                    logger.warning("Social Security Number should be 13 digits.\n");
-
-                }
-
-            } else if (!ssn.matches("^[0-9]+$")) {
-                logger.warning("Invalid Social Security Number");
-
-            }
 
 
-            logger.info("Email address: " );
-            String email = scanner.nextLine();
-            if (email == null || email.isEmpty()) {
-                logger.warning("This field should not be empty!\n");
+            String firstName= null;
+             do{
+                 logger.info("First name: " );
+                 firstName=scanner.nextLine();
 
-            } else if (!email.matches("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")) {
-                logger.warning("This field is invalid\n");
+                 if (firstName == null || firstName.isEmpty()) {
+                     logger.warning(emptyMessage);
+                     userInputCorrect =false;
 
-            }
+                 } else if (!firstName.matches("^[a-zA-Z]+$")) {
+                     logger.warning(invalidMessage);
+                     userInputCorrect =false;
 
-            logger.info("Username: " );
-            String username = scanner.nextLine();
-
-            if (username == null || username.isEmpty()) {
-                logger.warning("This field should not be empty!\n");
-
-            } else if (!username.matches("^[a-zA-Z0-9]+$")) {
-                logger.warning("This field is invalid\n");
-
-            }
+                 }else
+                     userInputCorrect= true;
+             }while (!userInputCorrect);
 
 
-            logger.info("Password - should contain at least one capital letter and one digit: " );
-            String password = scanner.nextLine();
-            if (password == null || password.isEmpty()) {
-                logger.warning("This field should not be empty!\n");
 
-            } else if (password.matches("^[a-zA-Z0-9]+$")) {
-                if ((password.length() < 6)) {
-                    logger.warning("Password is too short. It should contain at least 6 characters.\n");
+            String lastName;
+            do{
+                logger.info("Last name: " );
+                lastName = scanner.nextLine();
 
-                }
+                if (lastName == null || lastName.isEmpty()) {
+                    logger.warning(emptyMessage);
+                    userInputCorrect= false;
 
-            } else if (!password.matches("^[0-9]+$")) {
-                logger.warning("Invalid Password");
-            }
+                } else if (!lastName.matches("^[a-zA-Z]+$")) {
+                    logger.warning(invalidMessage);
+                    userInputCorrect= false;
+                } else
+                    userInputCorrect=true;
+
+            }while(!userInputCorrect);
+
+
+            String ssn;
+            do {
+                logger.info("Social Security Number: " );
+                ssn = scanner.nextLine();
+
+                if (ssn == null || ssn.isEmpty()) {
+                    logger.warning(emptyMessage);
+                    userInputCorrect= false;
+
+                } else if (ssn.matches("^[0-9]+$")) {
+                    if (!(ssn.length() == 13)) {
+                        logger.warning("Social Security Number should be 13 digits.\n");
+                    }
+                    userInputCorrect= false;
+
+                } else if (!ssn.matches("^[0-9]+$")) {
+                    logger.warning(invalidMessage);
+                    userInputCorrect= false;
+
+                } else
+                    userInputCorrect=true;
+
+            } while(!userInputCorrect);
+
+
+
+
+            String email;
+            do{
+                logger.info("Email address: " );
+                email = scanner.nextLine();
+
+                if (email == null || email.isEmpty()) {
+                    logger.warning(emptyMessage);
+                    userInputCorrect=false;
+
+                } else if (!email.matches("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")) {
+                    logger.warning(invalidMessage);
+                    userInputCorrect=false;
+
+                } else
+                    userInputCorrect= true;
+
+            }while(!userInputCorrect);
+
+
+            String username;
+            do{
+                logger.info("Username: " );
+                username = scanner.nextLine();
+
+                if (username == null || username.isEmpty()) {
+                    logger.warning(emptyMessage);
+                    userInputCorrect=false;
+
+                } else if (!username.matches("^[a-zA-Z0-9]+$")) {
+                    logger.warning(invalidMessage);
+                    userInputCorrect=false;
+
+                }else
+                    userInputCorrect=true;
+
+            }while(!userInputCorrect);
+
+
+
+            String password;
+            do{
+                logger.info("Password - should contain at least one capital letter and one digit: " );
+                password = scanner.nextLine();
+
+                if (password == null || password.isEmpty()) {
+                    logger.warning(emptyMessage);
+                    userInputCorrect=false;
+
+                } else if (password.matches("^[a-zA-Z0-9]+$")) {
+                    if (password.length() < 6) {
+                        logger.warning("Password is too short. It should contain at least 6 characters.\n");
+                        userInputCorrect=false;
+                    }
+
+                } else if (!password.matches("^[0-9]+$")) {
+                    logger.warning(invalidMessage);
+                    userInputCorrect=false;
+
+                }else
+                    userInputCorrect= true;
+
+            }while(!userInputCorrect);
+
 
 
            Customer customer = new Customer(firstName, lastName, ssn, email, username, password);
