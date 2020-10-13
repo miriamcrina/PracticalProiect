ae8875361497e6bea14b3ed26c7b39cb6fd9822e
Miriam <miriamcrina@yahoo.com>
2020-10-13 18:14:35 +0300

    13/10/20 -associations + customer dao + logIn method beginning

---
+++ BankingApp/src/main/java/com/sda/bankingApp/Repository/LogIn.java
@@ -0,0 +1,69 @@
+package com.sda.bankingApp.Repository;
+
+import com.sda.bankingApp.Entities.Customer;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LogIn {
+
+    private static final Logger logger = Logger.getLogger(Register.class.getName());
+
+
+
+    public void loginCheck() {
+        CustomerDao customerDao = new CustomerDao();
+        Customer customerToValidate;
+        boolean login = false;
+        int logcounter = 0;
+        String username;
+
+
+        while (logcounter < 3 && login == false) {
+
+            logger.info("Please Enter Username:");
+            Scanner userscanner = new Scanner(System.in);
+            username = userscanner.nextLine();
+            customerToValidate = customerDao.findByUsername(username);
+
+
+            if ("".equals(username)) {
+                logger.warning("Username field cannot be empty");
+            } else if ( customerToValidate == null) {
+                logger.warning("User not found");
+                logcounter++;
+                logger.warning(3 - (logcounter) + " login attempts remaining");
+            } else {
+
+                System.out.println("-------------------");
+                while (logcounter < 3 && login == false) {
+                    logger.info("Please Enter Password:");
+                    Scanner passscanner = new Scanner(System.in);
+                    String password = passscanner.nextLine();
+                    long customerIdByUsername = customerDao.findIdByUsername(username);
+                    long customerIdByPassword = customerDao.findIdByPassword(password);
+
+
+                    if ("".equals(password)) {
+                        logger.warning("This filed cannot be empty");
+
+                    } else if (customerIdByUsername != customerIdByPassword) {
+                        logger.warning("Wrong password! Please try again");
+                        logcounter++;
+                        logger.warning(3 - (logcounter) + " login attempts remaining");
+
+                    } else if (customerIdByUsername == customerIdByPassword) {
+                        logger.info("LOGIN SUCCESSFUL");
+                        login = true;
+                        break;
+
+                    }
+
+                }
+            }
+
+        }
+
+}
+}
